4fe67599fd3431c1eff0384779d64166fa3ed515
LXSCA <lucas.edr27@gmail.com>
2024-06-24 19:22:01 -0300

    feat: Adição de data na classe

--- NotaFiscal.Api/Models/Nota.cs
+++ NotaFiscal.Api/Models/Nota.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Models
 {
     public class Nota
     {
+        [Key]
         public int NotaFiscalId { get; set; }
         public string NumeroNf { get; set; }
         public decimal ValorTotal { get; set; }
+        public DateTime DataNf { get; set; }
         public string CnpjEmissorNf { get; set; }
         public string CnpjDestinatarioNf { get; set; }
     }
