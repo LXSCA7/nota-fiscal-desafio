af473d59a60d5380ba66c6bb8084ed1cbf69d0c1
LXSCA <lucas.edr27@gmail.com>
2024-06-25 17:44:06 -0300

    fix: erro de verificação no cnpj

--- NotaFiscal.Api/Models/Cnpj.cs
+++ NotaFiscal.Api/Models/Cnpj.cs
@@ -21,24 +21,14 @@ namespace NotaFiscal.Api.Models
 
         public static string FormataCnpj(string cnpj)
         {
-            StringBuilder newCnpj = new();
-            for (int i = 0; i < 14; i++)
-            {
-                if (i == 2 || i == 5)
-                    newCnpj.Append('.');
-                if (i == 8)
-                    newCnpj.Append('/');
-                if (i == 12)
-                    newCnpj.Append('-');
-
-                newCnpj.Append(cnpj[i]);
-            }
-            return newCnpj.ToString();
+            return Convert.ToUInt64(cnpj).ToString(@"00\.000\.000\/0000\-00");
         }
         public static bool VerificaCnpj(string cnpj)
-        {     
-            int[] digitos = new int[cnpj.Length];
+        {
+            if (cnpj.Length != 14)
+                return false;
 
+            int[] digitos = new int[14];
             for (int i = 0; i < cnpj.Length; i++)
             {
                 digitos[i] = int.Parse(cnpj[i].ToString());
@@ -47,7 +37,7 @@ namespace NotaFiscal.Api.Models
             // soma do primeiro digito verificador (X) xx.xxx.xxx/xxxx-Xx
             int soma = 0;
             int peso = 5;
-            for (int i = 0; i < cnpj.Length - 2; i++)
+            for (int i = 0; i < 12; i++)
             {
                 soma += digitos[i] * peso;
                 peso--;
@@ -60,7 +50,7 @@ namespace NotaFiscal.Api.Models
             // soma do primeiro digito verificador (X) xx.xxx.xxx/xxxx-xX
             soma = 0;
             peso = 6;
-            for (int i = 0; i < cnpj.Length - 2; i++)
+            for (int i = 0; i < 13; i++)
             {
                 soma += digitos[i] * peso;
                 peso--;
@@ -69,10 +59,9 @@ namespace NotaFiscal.Api.Models
             }
 
             resto = soma % 11;
-            int dv2 = resto > 2 ? 0 : 11 - resto;
+            int dv2 = resto < 2 ? 0 : 11 - resto;
 
             return digitos[12] == dv1 && digitos[13] == dv2;
         }
     }
-
 }
