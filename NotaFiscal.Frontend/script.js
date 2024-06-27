a4693f0199090f82bf55e81eefadcf877b41727d
LXSCA <lucas.edr27@gmail.com>
2024-06-27 13:15:31 -0300

    feat: Adição da busca de nota fiscal por ID.

--- NotaFiscal.Frontend/script.js
+++ NotaFiscal.Frontend/script.js
@@ -1,12 +1,49 @@
 const url = "http://localhost:5139/api/NotaFiscal";
-const urlBusca = "http://localhost:5139/buscarid/";
 let notas = [];
+
+function _displayItems(data) {
+    if (!data) {
+        document.getElementById("numero").value = "";
+        document.getElementById("valor").value = "";
+        document.getElementById("data").value = "";
+        document.getElementById("cnpjEmissor").value = "";
+        document.getElementById("cnpjDestinatario").value = "";
+        return;   
+    }
+
+    document.getElementById("numero").value = data.numeroNf;
+    document.getElementById("valor").value = data.valorTotal;
+    document.getElementById("data").value = data.dataNf;
+    document.getElementById("cnpjEmissor").value = data.cnpjEmissorNf;
+    document.getElementById("cnpjDestinatario").value = data.cnpjDestinatarioNf;
+    document.getElementById("encontrada").classList.remove("some");
+    document.getElementById("nao_encontrada").classList.add("some");
+}
 
 function listNotas() {
     fetch(url)
         .then(response => response.json())
         .then(data => _displayItems(data))
         .catch(error => console.error("Nota não encontrada.", error))
+}
+
+function buscarId() {
+    let id = document.getElementById("id_nota").value;
+
+    let fetchUrl = `${url}/buscarid/${id}`;
+
+    fetch(fetchUrl)
+        .then(response => {
+            if (!response.ok) {
+                _displayItems(null);
+                document.getElementById("nao_encontrada").classList.remove("some");
+                document.getElementById("encontrada").classList.add("some");
+                throw new Error("ID não encontrado.");
+            }
+            return response.json();
+        })
+        .then(data => _displayItems(data))
+        .catch(error => console.error("Erro ao buscar id", error));
 }
 
 function addNota() {
